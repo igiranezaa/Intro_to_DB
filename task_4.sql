1aa1ec86a9fa58249285c9d3a13eba3efc2020dc
igiranezaa <163933994+igiranezaa@users.noreply.github.com>
2025-10-16 11:47:56 +0200

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -11,5 +11,5 @@ SELECT
     COLUMN_DEFAULT AS 'Default',
     EXTRA AS 'Extra'
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' 
+WHERE TABLE_SCHEMA = 'alx_book_store'
   AND TABLE_NAME = 'Books';
